62fcbf87d530a94b6f59b0ebc05b236875ed3456
Alex Laptop Sabre <alexchoin@gmail.com>
2023-02-04 11:17:42 -0400

    font added, advisor placeholder scene added, advisor node is added into the player node

---
+++ advisorScene.gd
@@ -0,0 +1,14 @@
+extends Spatial
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$textMessage.visible_characters = 0
+
+func _on_Timer_timeout():
+	$textMessage.visible_characters += 1
