9e2d6f0c672741ff9286e1f38da667f53b13b05f
tehdi <tehdi@users.noreply.github.com>
2023-02-04 12:27:53 -0400

    simplified call from mural to interaction handler, and added more details

--- Mural/mural.gd
+++ Mural/mural.gd
@@ -1,10 +1,12 @@
 extends Node
 
+export(String) var mural_id
+export(String) var site_id
+
 func _on_Area_body_entered(body):
 	if body.is_in_group("player"):
-		$"/root/MuralInteraction".approach_mural(self.get_parent().name)
-
+		MuralInteraction.approach_mural(site_id, mural_id)
 
 func _on_Area_body_exited(body):
 	if body.is_in_group("player"):
-		$"/root/MuralInteraction".leave_mural(self.get_parent().name)
+		MuralInteraction.leave_mural(site_id, mural_id)
--- Mural/mural_interaction.gd
+++ Mural/mural_interaction.gd
@@ -1,7 +1,7 @@
 extends Node
 
-func approach_mural(mural):
-	print("player is approaching " + mural)
+func approach_mural(site_id: int, mural_id: int):
+	print("player is approaching mural {mural_id} at site {site_id}".format({"mural_id": mural_id, "site_id": site_id}))
 
-func leave_mural(mural):
-	print("player is leaving the area of " + mural)
+func leave_mural(site_id: int, mural_id: int):
+	print("player is leaving the area of mural {mural_id} at site {site_id}".format({"mural_id": mural_id, "site_id": site_id}))
