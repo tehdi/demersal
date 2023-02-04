9d17e42a01c8b37ae36ef95202733c63e5236fa9
tehdi <tehdi@users.noreply.github.com>
2023-02-04 09:39:43 -0400

    PoC for calling global functions when player approaches and leaves mural sites

--- Mural/mural.gd
+++ Mural/mural.gd
@@ -1,17 +1,10 @@
-extends MeshInstance
+extends Node
+
+func _on_Area_body_entered(body):
+	if body.is_in_group("player"):
+		$"/root/MuralInteraction".approach_mural(self.get_parent().name)
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-var observed: bool = false
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_Area_body_exited(body):
+	if body.is_in_group("player"):
+		$"/root/MuralInteraction".leave_mural(self.get_parent().name)
---
+++ Mural/mural_interaction.gd
@@ -0,0 +1,7 @@
+extends Node
+
+func approach_mural(mural):
+	print("player is approaching " + mural)
+
+func leave_mural(mural):
+	print("player is leaving the area of " + mural)
