f03d31e7516188f253edfe06a8f3c03d46bd0f19
tehdi <tehdi@users.noreply.github.com>
2023-02-05 12:03:59 -0400

    doing site tag things

--- SiteTagging.gd
+++ SiteTagging.gd
@@ -2,8 +2,16 @@ extends Node
 
 var tag_class = load("res://TagClass.gd")
 var mural_class = load("res://MuralClass.gd")
+var site_class = load("res://SiteClass.gd")
 
-var sites_unlocked = {}
+var sites_unlocked = {
+	"site_a": site_class.new("Site A", null,
+		["tag_pillars", "tag_walls", "tag_houses", "tag_towers", "tag_swords", "tag_pottery"]),
+	"site_b": site_class.new("Site B", null,
+		["tag_huts", "tag_ziggurats", "tag_spears"]),
+	"site_c": site_class.new("Site C", null,
+		["tag_buildings", "tag_asphalt", "tag_roads", "tag_lamps", "tag_electricity"])
+}
 
 var LOCKED_BY_DEFAULT = true
 var tags_all = {
@@ -15,10 +23,12 @@ var tags_all = {
 	"tag_cheese": tag_class.new("Cheese"),
 	"tag_astronomy": tag_class.new("Astronomy"),
 	"tag_literacy": tag_class.new("Literacy"),
+	"tag_pillars": tag_class.new("Pillars"),
 	"tag_textiles": tag_class.new("Textiles"),
 	"tag_trade": tag_class.new("Trade Routes"),
 	"tag_games": tag_class.new("Games"),
 	"tag_industry": tag_class.new("Industry"),
+	"tag_spears": tag_class.new("Spears"),
 	"tag_holidays": tag_class.new("Public Holidays"),
 	"tag_pets": tag_class.new("Pets"),
 	"tag_arrowheads": tag_class.new("Arrowheads"),
@@ -26,10 +36,12 @@ var tags_all = {
 	"tag_skeletons": tag_class.new("Humanoid Skeletons"),
 	"tag_paganism": tag_class.new("Paganism"),
 	"tag_huts": tag_class.new("Huts"),
+	"tag_houses": tag_class.new("Houses"),
 	"tag_ziggurats": tag_class.new("Ziggurats"),
 	"tag_pottery": tag_class.new("Pottery"),
 	"tag_towers": tag_class.new("Towers"),
 	"tag_statues": tag_class.new("Statues"),
+	"tag_buildings": tag_class.new("Buildings"),
 	"tag_glyphs": tag_class.new("Glyphs"),
 	"tag_farm_animals": tag_class.new("Farm Animals"),
 	"tag_theistic": tag_class.new("Theistic"),
@@ -38,6 +50,7 @@ var tags_all = {
 	"tag_bronze": tag_class.new("Bronze"),
 	"tag_wheels": tag_class.new("Wheels"),
 	"tag_military": tag_class.new("Military"),
+	"tag_swords": tag_class.new("Swords"),
 	"tag_horses": tag_class.new("Horses"),
 	"tag_iron": tag_class.new("Iron"),
 	"tag_shipbuilding": tag_class.new("Shipbuilding"),
@@ -51,15 +64,12 @@ var tags_all = {
 
 var murals_unlocked = {}
 var murals_all = {
-	# "id": name, icon, wanted tags, unlockable tags
+	# "id": name, icon, unlockable tags
 	"mural_hero": mural_class.new("Hero", null,
-		[], # wants
 		["tag_monarch"]), # unlocks
 	"mural_monarch": mural_class.new("Monarch", null,
-		[], # wants
 		["tag_deity"]), # unlocks
 	"mural_deity": mural_class.new("Deity", null,
-		[], # wants
 		["tag_hero"]) # unlocks
 }
 
