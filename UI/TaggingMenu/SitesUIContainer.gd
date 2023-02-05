f83cde3c86272437acdc2357f23dce05ccfc6b83
tehdi <tehdi@users.noreply.github.com>
2023-02-05 11:45:46 -0400

    more mural removal / site tagging menu clean-up

--- UI/TaggingMenu/SitesUIContainer.gd
+++ UI/TaggingMenu/SitesUIContainer.gd
@@ -1,37 +1,27 @@
 extends HBoxContainer
 
 export (PackedScene) var site_ui_prefab
-export (NodePath) var mural_container_node_path
 export (NodePath) var tag_container_node_path
 
-var mural_container
 var tag_container
 
 func _ready():
-	mural_container = get_node(mural_container_node_path)
 	tag_container = get_node(tag_container_node_path)
-
 
 func setup_sites_ui():
 	# Remove all existing 
 	for child in get_children():
 		child.queue_free()
-	
+
 	# Find all the sites from $Global
 	for site_key in SiteTagging.sites_unlocked:
 		var site = SiteTagging.sites_unlocked[site_key]
-		
 		add_site_ui(site_key, site)
-		
-
 
 func add_site_ui(site_id, site):
 	var site_instance = site_ui_prefab.instance()
-	
-	site_instance.setup_ui(site_id, site.display_name, site.icon)
-	
+	site_instance.setup_ui(site_id, site.icon)
 	site_instance.connect("selected", self, "on_site_selected")
-	
 	add_child(site_instance)
 
 func on_site_selected(site_id):
