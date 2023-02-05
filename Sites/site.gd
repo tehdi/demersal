e95316cf8658fa4f78a1cd2a304eddd0d283d6de
tehdi <tehdi@users.noreply.github.com>
2023-02-05 15:44:18 -0400

    fade music back to base environmental when exiting a site

--- Sites/site.gd
+++ Sites/site.gd
@@ -8,10 +8,10 @@ var discovered: bool = false
 func _on_Area_body_entered(body):
 	if body.is_in_group("player"):
 		BackgroundMusic.crossfade(track_index)
-		
 		if !discovered:
 			discovered = true
 			SiteTagging._on_site_site_discovered(site_id)
 
 func _on_Area_body_exited(body):
-	pass
+	if body.is_in_group("player"):
+		BackgroundMusic.crossfade(1)
