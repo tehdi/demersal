b420b7a3a84c6858e3a6bc90c46ac7f9389503bd
jeffrey_fillingham <jeffreyf@cs.dal.ca>
2023-02-05 14:12:09 -0400

    Tagging will bring up the advisor if all tagged correctly (hopefully)

--- UI/TaggingMenu/TagMenuControl.gd
+++ UI/TaggingMenu/TagMenuControl.gd
@@ -6,6 +6,7 @@ func _process(delta):
 		get_parent().move_child(self, 0)
 		get_tree().paused = !get_tree().paused
 		$CanvasLayer/TaggingMenu.visible = !$CanvasLayer/TaggingMenu.visible
+		$CanvasLayer/TaggingMenu._ready()
 		
 		if Input.get_mouse_mode() == Input.MOUSE_MODE_VISIBLE:
 			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
--- UI/TaggingMenu/TaggingMenu.gd
+++ UI/TaggingMenu/TaggingMenu.gd
@@ -9,3 +9,6 @@ func _enter_tree():
 func update_sites():
 	# Tell the SitesContainer to update
 	$PanelContainer/VBoxContainer/SitesContainer.setup_sites_ui()
+
+func sites_correctly_tagged():
+	$"Tagging Advisor Cut Scenes".play_advisor_correct_tags()
