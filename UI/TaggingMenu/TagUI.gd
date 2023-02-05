adc794af3d0a77c28a374a5de37a7a782aa01c1c
jeffrey_fillingham <jeffreyf@cs.dal.ca>
2023-02-05 14:55:54 -0400

    Advisor and correct tags in tag menu work now

--- UI/TaggingMenu/TagUI.gd
+++ UI/TaggingMenu/TagUI.gd
@@ -12,6 +12,8 @@ func setup_tag(site_id, tag_id, tag, toggle):
 	self.tag_id = tag_id
 
 func _on_CheckBox_pressed():
+	var was_correctly_tagged = SiteTagging.has_enough_correct_tags()
+	
 	var selected_tag_ids = SiteTagging.sites_unlocked[site_id].selected_tag_ids
 	if $CheckBox.pressed and not tag_id in selected_tag_ids:
 		selected_tag_ids.append(tag_id)
@@ -19,5 +21,10 @@ func _on_CheckBox_pressed():
 		selected_tag_ids.erase(tag_id)
 	var sites_correctly_tagged = SiteTagging.has_enough_correct_tags()
 	
-	if sites_correctly_tagged:
-		get_tree().get_root().find_node("TaggingMenu").sites_correctly_tagged()
+	if !was_correctly_tagged && sites_correctly_tagged:
+		var tree = get_tree()
+		var root = tree.get_root()
+		var tagginMenu = get_parent().get_parent().get_parent().get_parent().get_parent()
+		tagginMenu.sites_correctly_tagged()
+		var doorPlayer = get_node("/root/baseScene/doorPlayer")
+		doorPlayer.play("doorOpen")
